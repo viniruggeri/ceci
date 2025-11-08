b1d9c63af44c7ec57df8b88b3000216a31195eec
viniruggeri <vliraruggeri@gmail.com>
2025-11-07 21:29:20 -0300

    "removed --startup.sh"

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Download spaCy model if not present
-python -m spacy download pt_core_news_sm --quiet || true
-
-# Start application
-uvicorn app:app --host 0.0.0.0 --port 8000 --workers 1
